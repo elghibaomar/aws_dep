9fe33b7fcd288b6539af8ed852b71da9b9fa48f4
elghibaomar <elghibaomar@gmail.com>
2022-10-06 14:13:08 +0100

    t1

--- services/api/main.py
+++ services/api/main.py
@@ -1,28 +1,46 @@
-import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
+# from .auth import AuthHandler
+# from .schemas import AuthDetails
+import auth, schemas
+
 
 app = FastAPI()
 
-# @app.get("/")
-# def home():
-#     return {"Hello": "World"}
 
-# @app.get("/")
-# def home():
-#     return {"Hello": "GET"}
+auth_handler = auth.AuthHandler()
+users = []
 
-# @app.post("/")
-# def home_post():
-#     return {"Hello": "POST"}
+@app.post('/register', status_code=201)
+def register(auth_details: schemas.AuthDetails):
+    if any(x['username'] == auth_details.username for x in users):
+        raise HTTPException(status_code=400, detail='Username is taken')
+    hashed_password = auth_handler.get_password_hash(auth_details.password)
+    users.append({
+        'username': auth_details.username,
+        'password': hashed_password    
+    })
+    return
 
-# @app.route("/employee/<int:id>/")
-# def home():
-#     return {"id": id}
+
+@app.post('/login')
+def login(auth_details: schemas.AuthDetails):
+    user = None
+    for x in users:
+        if x['username'] == auth_details.username:
+            user = x
+            break
+    
+    if (user is None) or (not auth_handler.verify_password(auth_details.password, user['password'])):
+        raise HTTPException(status_code=401, detail='Invalid username and/or password')
+    token = auth_handler.encode_token(user['username'])
+    return { 'token': token }
 
 
-@app.get("/employee")
-def home(department: str):
-    return {"department": department}
+@app.get('/unprotected')
+def unprotected():
+    return { 'hello': 'world' }
 
-if __name__ == "__main__":
-    uvicorn.run("hello_world_fastapi:app")
+
+@app.get('/protected')
+def protected(username=Depends(auth_handler.auth_wrapper)):
+    return { 'name': username }
